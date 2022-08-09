041a37445ea3f069528c25bdc86c561d5deb5cdd
Eric20Junior <eric20junior@gmail.com>
2022-08-09 16:14:18 +0100

    tasked completed

---
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -0,0 +1,42 @@
+const fs = require('fs');
+
+module.exports = function countStudents(path) {
+  let csv = [];
+  const msg1 = 'Number of students:';
+
+  try {
+    csv = fs.readFileSync(path, 'utf8');
+  } catch (err) {
+    throw new Error('Cannot load the database');
+  }
+
+  // Created 2D array
+  csv = csv
+    .toString()
+    .split('\n')
+    .map((e) => e.trim(e))
+    .map((e) => e.split(',').map((e) => e.trim()))
+    .map((e) => e.filter(Boolean))
+    .filter((e) => e.length > 0);
+
+  const headers = csv[0];
+  csv.shift();
+
+  // Create array of student objects
+  const result = csv.map((value) => (
+    value.reduce((acc, data, index) => (
+      { ...acc, [headers[index]]: data }), {})));
+
+  console.log(`${msg1} ${result.length}`);
+
+  // Get count of students by field, list firstname in each field.
+  // Use array.reduce().
+  // See https://learnwithparam.com/blog/how-to-group-by-array-of-objects-using-a-key/
+
+  // Sone fooling around before finding above reduce()
+  // console.log(result);
+  // for (const row of result) {
+  //   console.log(row.field);
+  // }
+  // console.log(result[1].field, result[1].firstname);
+};
